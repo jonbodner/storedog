33a5d8467d22f242e6d72d816b741c6cc1160ed8
Jon Bodner <jon.bodner@datadoghq.com>
2023-07-17 14:50:02 -0400

    remove more dd stuff

--- services/backend/config/initializers/datadog-tracer.rb
+++
@@ -1,6 +0,0 @@
-Datadog.configure do |c|
-  c.env = ENV['DD_ENV'] || 'development'
-  c.service = ENV['DD_SERVICE'] || 'store-backend'
-  c.tracing.sampling.default_rate = 1.0
-  c.profiling.enabled = true
-end
